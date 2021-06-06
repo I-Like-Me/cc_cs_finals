58f1d229f3457ec49c815acc5c7b8ae92834e58b
cassgolkin@gmail.com <cassgolkin@gmail.com>
2021-06-06 15:38:47 -0400

    Added if..else to have different inputs do different things.

--- idea_test/left_right_input.py
+++ idea_test/left_right_input.py
@@ -1,2 +1,15 @@
-direction = input("Do you go 'left' or 'right'?: ")
-print("Character went " + direction + ".")
+
+print("\n")
+print("You come to a fork in the path.")
+print("\n")
+direction = input("Do you take the 'left' path or the 'right' path?: ")
+
+if direction == "left":
+  print("\n")
+  print("You went " + direction + " and stumbled upon a bandit's hideout.")
+elif direction == "right":
+  print("\n")
+  print("You went " + direction + " and discovered a cave filled with living skelitons.")
+else:
+  print("\n")
+  print("You went off the path and got lost.")
